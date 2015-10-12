f27688a738a76b32d28271f9c7994cc092a8d59c
sonicyang <sonic.tw.tp@gmail.com>
2015-10-12 16:28:05 +0800

    Add dec for most of the crads

--- static/js/card.js
+++ static/js/card.js
@@ -4,37 +4,40 @@ $(document).ready(function () {
         $(this).css('height', height+'px');
 
         var infobox = $(this);
-        if($(this).children(".inf").css('opacity') == 1){
-            $(this).children(".inf").animate({opacity: 0},
+        if($(this).find(".inf").css('opacity') == 1){
+            $(this).find(".inf").animate({opacity: 0},
                                              {duration: 400, step: function(now, fx){
                                                                         infobox.css('transform','rotateY('+(360 - now*360)+'deg)');
                                                                     }
                                              });
         }else{
-            $(this).children(".inf").animate({opacity: 1},
+            $(this).find(".inf").animate({opacity: 1},
                                              {duration: 400, step: function(now, fx){
                                                                         infobox.css('transform','rotateY('+(360 - now*360)+'deg)');
                                                                     }
                                              });
         }
 
-        $(this).children(".inf").toggle();
-        $(this).children(".des").toggle();
+        $(this).find(".inf").toggle();
+        $(this).find(".des").toggle();
+        $(this).find(".info-box-image").toggle();
     });
 
     $(".info-box").hover(function(){
-        if($(this).css('opacity') == 1){
-            $(this).animate({opacity: 0.95},
-                                             {duration: 100, step: function(now, fx){
-                                                                        $(this).css('transform','scale('+now+')');
-                                                                    }
-                                             });
-        }else{
-            $(this).animate({opacity: 1},
-                                             {duration: 100, step: function(now, fx){
-                                                                        $(this).css('transform','scale('+now+')');
-                                                                    }
-                                             });
+        if($(this).find(".inf").length > 0){
+            if($(this).css('opacity') == 1){
+                $(this).animate({opacity: 0.95},
+                                                 {duration: 100, step: function(now, fx){
+                                                                            $(this).css('transform','scale('+now+')');
+                                                                        }
+                                                 });
+            }else{
+                $(this).animate({opacity: 1},
+                                                 {duration: 100, step: function(now, fx){
+                                                                            $(this).css('transform','scale('+now+')');
+                                                                        }
+                                                 });
+            }
         }
     });
 
